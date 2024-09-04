e3fa8966cad04328721ca1940086586702596b19
Maor <maorsa9@gmail.com>
2024-09-04 19:54:19 +0300

    finished with dashboard page

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -7,11 +7,11 @@ import {
   TrendingDown,
   TrendingUp,
 } from "lucide-react";
-// import CardExpenseSummary from "./CardExpenseSummary";
+import CardExpenseSummary from "./CardExpenseSummary";
 import CardPopularProducts from "./CardPopularProducts";
 import CardPurchaseSummary from "./CardPurchaseSummary";
 import CardSalesSummary from "./CardSalesSummary";
-// import StatCard from "./StatCard";
+import StatCard from "./StatCard";
 
 const Dashboard = () => {
   return (
@@ -19,8 +19,8 @@ const Dashboard = () => {
       <CardPopularProducts />
       <CardSalesSummary />
       <CardPurchaseSummary />
-      {/* <CardExpenseSummary /> */}
-      {/* <StatCard
+      <CardExpenseSummary />
+      <StatCard
         title="Customer & Expenses"
         primaryIcon={<Package className="text-blue-600 w-6 h-6" />}
         dateRange="22 - 29 October 2023"
@@ -76,7 +76,7 @@ const Dashboard = () => {
             IconComponent: TrendingDown,
           },
         ]}
-      /> */}
+      />
     </div>
   );
 };
